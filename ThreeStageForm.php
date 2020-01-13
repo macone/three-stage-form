3cc453774a21548c60e10fce7596c9caaff57d71
macone <maciek@checkme.pl>
2020-01-14 00:02:39 +0100

    setup

---
+++ ThreeStageForm.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @wordpress-plugin
+ * Plugin Name:       Three Stage Form
+ */
+
+defined( 'ABSPATH' ) or die( 'Direct script access disallowed.' );
+
+define( 'ERW_WIDGET_PATH', plugin_dir_path( __FILE__ ) . '/3s-form/' );
+define( 'ERW_ASSET_MANIFEST', ERW_WIDGET_PATH . '/build/asset-manifest.json' );
+define( 'ERW_INCLUDES', plugin_dir_path( __FILE__ ) . '/includes' );
+
+require_once( ERW_INCLUDES . '/enqueue.php' );
---
+++ includes/enqueue.php
@@ -0,0 +1,4 @@
+<?php
+// This file enqueues scripts and styles
+
+defined( 'ABSPATH' ) or die( 'Direct script access disallowed.' );
