5e4a77e5a984ac008332e762f9b3a2d33a8e93a3
macone <maciek@checkme.pl>
2020-01-15 03:14:07 +0100

    stage2

--- 3sform/src/components/MainForm.js
+++ 3sform/src/components/MainForm.js
@@ -2,10 +2,6 @@ import React, { Component } from 'react'
 
 import countries from '../assets/ISO_3166-1_countries_pl.json'
 
-import Step1 from './Step1'
-import Step2 from './Step2'
-import Step3 from './Step3'
-
 export default class MainForm extends Component {
 	constructor(props) {
 		super(props);
@@ -15,7 +11,7 @@ export default class MainForm extends Component {
 			emailAddress: "",
 			bestCountry: "",
 			favouritePet: "",
-			luckyNumber: null,
+			luckyNumber: "",
 			legalConsent1: false,
 			legalConsent2: false,
 			error: {
@@ -29,10 +25,185 @@ export default class MainForm extends Component {
 				legalConsent2: "",
 			},
 			finalValidate: false,
-			buttonText: "Dalej"
+			buttonText: "Dalej",
+			step: 2
 		};
 		this.handleChange = this.handleChange.bind(this);
-		this.validate = this.validate.bind(this);
+		this._validate = this._validate.bind(this);
+		this.validate = this.validate.bind(this)
+		this._next = this._next.bind(this)
+		this._prev = this._prev.bind(this)
+	}
+
+	_validate(name, value) {
+		console.log(name)
+		switch (name) {
+			case "firstName":
+				if (value.length < 2) {
+					this.setState(prevState => ({
+						error: {
+							...prevState.error,
+							firstName: 'Imię jest za krótkie'
+						}
+					}))
+					return false
+				} else if (/\d/.test(value)) {
+					this.setState(prevState => ({
+						error: {
+							...prevState.error,
+							firstName: 'Imię nie można zawierać cyfr'
+						}
+					}))
+					return false
+				} else {
+					this.setState(prevState => ({
+						error: {
+							...prevState.error,
+							firstName: ''
+						}
+					}))
+					return true
+				}
+			case "lastName":
+				if (value.length < 2) {
+					this.setState(prevState => ({
+						error: {
+							...prevState.error,
+							lastName: 'Nazwisko jest za krótkie'
+						}
+					}))
+					return false
+				} else if (/\d/.test(value)) {
+					this.setState(prevState => ({
+						error: {
+							...prevState.error,
+							lastName: 'Nazwisko nie można zawierać cyfr'
+						}
+					}))
+					return false
+				} else {
+					this.setState(prevState => ({
+						error: {
+							...prevState.error,
+							lastName: ''
+						}
+					}))
+					return true
+				}
+			case "emailAddress":
+				if (this.validateEmail(value)) {
+					this.setState(prevState => ({
+						error: {
+							...prevState.error,
+							emailAddress: ''
+						}
+					}))
+					return true
+				} else {
+					this.setState(prevState => ({
+						error: {
+							...prevState.error,
+							emailAddress: 'Adres e-mail jest nieprawidłowy'
+						}
+					}))
+					return false
+				}
+			case "bestCountry":
+				if (value !== "") {
+					this.setState(prevState => ({
+						error: {
+							...prevState.error,
+							bestCountry: ''
+						}
+					}))
+					return true
+				} else {
+					this.setState(prevState => ({
+						error: {
+							...prevState.error,
+							bestCountry: 'Wybierz państwo'
+						}
+					}))
+					return false
+				}
+		}
+		console.log(this.state)
+	}
+
+	_next() {
+		console.log(this.state)
+		let currentStep = this.state.step
+		if (currentStep === 1) {
+			this._validate("firstName", this.state.firstName)
+			this._validate("lastName", this.state.lastName)
+			this._validate("emailAddress", this.state.emailAddress)
+			if (this._validate("firstName", this.state.firstName) && this._validate("lastName", this.state.lastName) && this._validate("emailAddress", this.state.emailAddress)) {
+				currentStep = currentStep >= 2 ? 3 : currentStep + 1
+				this.setState({
+					step: currentStep
+				})
+			}
+		}
+		if (currentStep === 2) {
+			this._validate("bestCountry", this.state.bestCountry)
+			if (this._validate("bestCountry", this.state.bestCountry)) {
+				currentStep = currentStep >= 2 ? 3 : currentStep + 1
+				this.setState({
+					step: currentStep
+				})
+			}
+		}
+
+	}
+
+	_prev() {
+		let currentStep = this.state.step
+		currentStep = currentStep <= 1 ? 1 : currentStep - 1
+		this.setState({
+			step: currentStep
+		})
+	}
+
+	get previousButton() {
+		let currentStep = this.state.step;
+		if (currentStep !== 1) {
+			return (
+				<div className="cont-btn">
+					<div className="wrapper-btn">
+						<div className="tsf-bg-btn"></div>
+						<button
+							className="tsf-btn"
+							type="button" onClick={this._prev}>
+							Wstecz
+							</button>
+
+					</div>
+				</div>
+
+			)
+		}
+		return null;
+	}
+
+	get nextButton() {
+		let currentStep = this.state.step;
+
+		if (currentStep < 3) {
+			return (
+				<div className="cont-btn">
+					<div className="wrapper-btn">
+						<div className="tsf-bg-btn"></div>
+						<button
+							className="tsf-btn"
+							type="button" onClick={this._next}>
+							Dalej
+				</button>
+
+					</div>
+				</div>
+			)
+		}
+		return null;
 	}
 
 	validateEmail(email) {
@@ -45,80 +216,14 @@ export default class MainForm extends Component {
 		this.setState({ [event.target.name]: event.target.value });
 	}
 
-
 	validate(event) {
 		const { name, value } = event.target
-
-		console.log()
-		switch (name) {
-			case "firstName":
-				if (value.length < 2) {
-					this.setState(prevState => ({
-						error: {
-							...prevState.error,
-							firstName: 'Imię jest za krótkie'
-						}
-					}))
-				} else if (/\d/.test(value)) {
-					this.setState(prevState => ({
-						error: {
-							...prevState.error,
-							firstName: 'Imię nie można zawierać cyfr'
-						}
-					}))
-				} else {
-					this.setState(prevState => ({
-						error: {
-							...prevState.error,
-							firstName: ''
-						}
-					}))
-				}
-				break;
-			case "lastName":
-				if (value.length < 2) {
-					this.setState(prevState => ({
-						error: {
-							...prevState.error,
-							lastName: 'Nazwisko jest za krótkie'
-						}
-					}))
-				} else if (/\d/.test(value)) {
-					this.setState(prevState => ({
-						error: {
-							...prevState.error,
-							lastName: 'Nazwisko nie można zawierać cyfr'
-						}
-					}))
-				} else {
-					this.setState(prevState => ({
-						error: {
-							...prevState.error,
-							lastName: ''
-						}
-					}))
-				}
-				break;
-			case "emailAddress":
-				if (this.validateEmail(value)) {
-					this.setState(prevState => ({
-						error: {
-							...prevState.error,
-							emailAddress: ''
-						}
-					}))
-				} else {
-					this.setState(prevState => ({
-						error: {
-							...prevState.error,
-							emailAddress: 'Adres e-mail jest nieprawidłowy'
-						}
-					}))
-				}
-				break;
+		if (this) {
+			this._validate(name, value)
 		}
-		console.log(this.state)
+
 	}
+
 
 
 	render() {
@@ -126,7 +231,7 @@ export default class MainForm extends Component {
 			<div className="center-children">
 				<div className="tsf-form-parent">
 					<form className="tsf-form">
-						<section className="stage" id="stage-one">
+						<section className={this.state.step === 1 ? "stage" : "stage fade-out"} id="stage-one">
 							<div data-validate={this.state.error.firstName} className={this.state.error.firstName ? "wrap-input alert-validate" : "wrap-input"}>
 								<input className={
 									this.state.firstName ? 'inp has-val' : 'inp'
@@ -147,30 +252,49 @@ export default class MainForm extends Component {
 							</div>
 
 						</section>
-						<section className="stage" id="stage-two">
-							<p>Najbardziej polecane państwo do odwiedzenia?
-						<select value={this.state.bestCountry} onChange={this.handleChange} onBlur={this.validate} name="bestCountry">
+
+
+						<section className={this.state.step === 2 ? "stage" : "stage fade-out"} id="stage-two">
+							<div data-validate={this.state.error.bestCountry} className={this.state.error.bestCountry ? "alert-validate" : null}>Najbardziej polecane państwo do odwiedzenia?
+						<select className="select-box" value={this.state.bestCountry} onChange={this.handleChange} onBlur={this.validate} name="bestCountry">
 									{
 										Array.from(countries).map((val, ind) => {
 											return <option key={ind} value={val.name_pl}>{val.name_pl} </option>
 										})
 									}
 								</select>
-							</p>
-							<div>Wolisz psy czy koty?
-						<label><input type="radio" name="favouritePet" value="Koty" onChange={this.handleChange} />Koty </label>
-								<label><input type="radio" name="favouritePet" value="Psy" onChange={this.handleChange} />Psy </label>
 							</div>
-							<div>Szczęśliwa liczba<input type="number" placeholder="Liczba" name="luckyNumber" /></div>
+
+
+							<div>Wolisz psy czy koty?
+						<label className="radio"><input type="radio" name="favouritePet" value="Koty" onChange={this.handleChange} /><span>Koty</span> </label>
+								<label className='radio'><input type="radio" name="favouritePet" value="Psy" onChange={this.handleChange} /><span>Psy</span> </label>
+							</div>
+
+							<div data-validate={this.state.error.luckyNumber} className={this.state.error.luckyNumber ? "wrap-input alert-validate" : "wrap-input"}>
+
+								<input className={
+									this.state.luckyNumber ? 'inp has-val' : 'inp'
+								} type="number" name="luckyNumber" value={this.state.luckyNumber} onChange={this.handleChange} onBlur={this.validate} />
+								<span className="focus-input" data-placeholder="Szczęśliwa liczba"></span>
+							</div>
+
+
 						</section>
-						<section className="stage" id="stage-three">
+
+
+
+						<section className={this.state.step === 3 ? "stage" : "stage fade-out"} id="stage-three">
 							<div><label>Wyrażam zgodę nr 1 <input type="checkbox" name="legalConsent1" /></label></div>
 							<div><label>Wyrażam zgodę nr 2  <input type="checkbox" name="legalConsent2" /></label></div>
 							<input type="hidden" name="elqFormName" value="recruitment" />
 							<input type="hidden" name="elqSiteID" value="885580810" />
 						</section>
-						<button>Wstecz </button>
-						<button>{this.state.buttonText} </button>
+
+
+
+						{this.previousButton}
+						{this.nextButton}
 					</form >
 				</div>
 			</div>
